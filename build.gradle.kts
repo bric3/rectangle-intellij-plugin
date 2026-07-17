9b218d0b7ca9e12a712415e726a249a26bbc1912
Brice Dutheil <brice.dutheil@gmail.com>
2026-07-17 12:14:41 +0200

    fix: Properly set-up kotlin dependency for the IJ plugin
    kotlin.stdlib.default.dependency is false, as such the project needs
    to be told about kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,9 @@ repositories {
 
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
+  compileOnly(libs.kotlin.stdlib)
+  compileOnly(libs.kotlin.reflect)
+
   testImplementation(libs.junit)
 
   // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
